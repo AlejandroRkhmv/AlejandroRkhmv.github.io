1e8604c9f33bdbfb01a49d496e9fa4b244705711
Alexander-Rakhimov <alexanderrkhmv@gmail.com>
2023-03-28 16:30:59 +0400

    Create my presentation

---
+++ script.js
@@ -0,0 +1,9 @@
+// typing text animation script
+  var typed = new Typed(".whoAmI", {
+      strings: ["Husband", "Father", "iOS Developer", "Skydiver", "Fly Navigator","QA Engineer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      cursorChar: ' _',
+      smartBackspace: true,
+      loop: true
+  });
